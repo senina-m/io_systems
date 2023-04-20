8d650d9649eb3823a3e40fc3767c466831fd7abb
Senina Mariya <senina.m.m@yandex.ru>
2023-04-20 18:54:12 +0300

    report done + time.sh to check speeds

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,3 +1,4 @@
+
 #include <linux/bio.h>
 #include <linux/blk-mq.h>
 #include <linux/blkdev.h>
@@ -78,58 +79,70 @@ static PartTable def_part_table =
 		start_head: 0x4,
 		start_sec: 0x1,
 		start_cyl: 0x0,
-		part_type: 0x05, // extended partition type
+		part_type: 0x83,
 		end_sec: 0x20,
 		end_head: 0xB,
 		end_cyl: 0x9F,
 		abs_start_sec: 0x5000,
-		sec_in_part: 0xA000
+		sec_in_part: 0xA000 // 20Mbyte а не 9FFF?
+	},
+  {
+		boot_type: 0x00,
+		start_head: 0xC,
+		start_sec: 0x1,
+		start_cyl: 0x0,
+		part_type: 0x05, // extended partition type
+		end_sec: 0x20,
+		end_head: 0x13,
+		end_cyl: 0x9F,
+		abs_start_sec: 0xF000, // A000 + 5000
+		sec_in_part: 0xA000 // 20MByte а не 9FFF?
 	}
 };
 
-static unsigned int def_log_part_br_abs_start_sector[] = {0x5000, 0xA000};
+static unsigned int def_log_part_br_abs_start_sector[] = {0xF000, 0x14000};
 static const PartTable def_log_part_table[] =
 {
 	{
 		{
 			boot_type: 0x00,
-			start_head: 0x4,
-			start_sec: 0x2, 
-			start_cyl: 0x0, 
+			start_head: head4size(0xF000),
+			start_sec: sec4size(0xF000),
+			start_cyl: 0x0,
 			part_type: 0x83,
-			end_head: 0x7,
-			end_sec: 0x20,
+			end_head: head4size(0x13FFF),
+			end_sec: sec4size(0x13FFF),
 			end_cyl: 0x9F,
 			abs_start_sec: 0x1,
 			sec_in_part: 0x4FFF
 		},
-		{
+        {
 			boot_type: 0x00,
-			start_head: 0x8,
-			start_sec: 0x01,
-			start_cyl: 0x00,
+			start_head: head4size(0x14000),
+			start_sec: sec4size(0x14000), 
+			start_cyl: 0x0, 
 			part_type: 0x05,
-			end_head: 0xB,
-			end_sec: 0x20,
+			end_head: head4size(0x18FFFF),
+			end_sec: sec4size(0x18FFFF),
 			end_cyl: 0x9F,
 			abs_start_sec: 0x5000,
 			sec_in_part: 0x5000
 		}
 	},
-	{
-		{
-			boot_type: 0x00,
-			start_head: 0x8,
-			start_sec: 0x02,
-			start_cyl: 0x00,
-			part_type: 0x83,
-			end_head: 0xB,
-			end_sec: 0x20,
-			end_cyl: 0x9F,
-			abs_start_sec: 0x1,
-			sec_in_part: 0x4FFF
-		}
-	}
+    {
+      {
+		    boot_type: 0x00,
+		    start_sec: sec4size(0x14000),
+		    start_head: head4size(0x14000),
+		    start_cyl: 0x0,
+		    part_type: 0x83,
+		    end_head: head4size(0x18FFFF),
+		    end_sec: sec4size(0x18FFFF),
+		    end_cyl: 0x9F,
+		    abs_start_sec: 0x1,
+		    sec_in_part: 0x4FFF
+	    }    
+    }
 };
 
 static void copy_mbr(u8 *disk) {
